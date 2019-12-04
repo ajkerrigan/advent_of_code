cd40115b79e3dc3504584e16cae4d8f713ba4f99
AJ Kerrigan <akerrigan@mdsol.com>
2019-12-04 10:20:10 -0500

    Refactor day 4 part 2

--- day4_secure_container.py
+++ day4_secure_container.py
@@ -1,30 +1,26 @@
+from collections import Counter
+
+
 def parse_range(input):
-    start, end = input.split('-')
-    return (int(start), int(end)) 
+    start, end = input.split("-")
+    return (int(start), int(end))
+
 
 def is_valid_password(password):
     if list(password) != sorted(password):
         return False
+    c = Counter(password)
+    return 2 in c.values()
 
-    current_repeat_length = 1
-    repeat_lengths = set()
 
-    for i, char in enumerate(password[1:]):
-        if char == password[i]:
-            current_repeat_length += 1
-        else:
-            repeat_lengths.add(current_repeat_length)
-            current_repeat_length = 1
-    repeat_lengths.add(current_repeat_length)
-    return 2 in repeat_lengths
-        
 def matching_passwords(start, end):
-    possible_passwords = range(start, end+1)
-    return (
-        p for p in possible_passwords
-        if is_valid_password(str(p))
-    )
+    possible_passwords = range(start, end + 1)
+    return (p for p in possible_passwords if is_valid_password(str(p)))
+
+
+def main():
+    start, end = parse_range("359282-820401")
+    print(len(list(matching_passwords(start, end))))
 
 if __name__ == '__main__':
-    start, end = parse_range('359282-820401')
-    print(len(list(matching_passwords(start, end))))
+    main()
