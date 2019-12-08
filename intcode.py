26db714798d0d99b0feaac7bd8c316b7d6033dd7
AJ Kerrigan <akerrigan@mdsol.com>
2019-12-08 03:19:04 -0500

    Ugh day 7 part 2 works, but... not so pretty

--- intcode.py
+++ intcode.py
@@ -1,4 +1,7 @@
 import itertools
+import queue
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
 from collections import deque
 from dataclasses import dataclass, field
 from enum import IntEnum
@@ -46,7 +49,6 @@ class Program:
             8: Op(self.eq, 2, True),
             99: Op(END, 0, False),
         }
-        self.outputs = []
 
     @staticmethod
     def parse_ops(s):
@@ -57,8 +59,8 @@ class Program:
         return self._ops
 
     def store(self):
-        print(self.inputs)
-        return self.inputs.popleft()
+        # print(self.inputs)
+        return self.inq.get()
 
     def output(self, val):
         return val
@@ -89,17 +91,19 @@ class Program:
                 self.program[start_pos + 1 : start_pos + 1 + op.input_count]
             )
         ]
-        print((op, mode, start_pos, *inputs))
+        # print((op, mode, start_pos, *inputs))
         result = OpResult(op.func(*inputs))
-        if op.produces_output:
-            print(f'Storing {result} at position {start_pos + 1 + op.input_count}')
-            self.program[self.program[start_pos + 1 + op.input_count]] = result.value
-        elif result.value is not None:
-            self.outputs.append(result.value)
+        if result.value is not None:
+            if op.produces_output:
+                # print(f'Storing {result} at position {start_pos + 1 + op.input_count}')
+                self.program[self.program[start_pos + 1 + op.input_count]] = result.value
+            else:
+                self.outq.put(result.value)
         return result.jump
 
-    def run(self, *inputs):
-        self.inputs = deque(inputs)
+    def run(self, inq, outq):
+        self.inq = inq
+        self.outq = outq
         i = 0
         while True:
             mode, opcode = divmod(self.program[i], 100)
@@ -108,20 +112,36 @@ class Program:
                 break
             jump = self.exec(op, mode, i)
             i = jump or i + 1 + op.input_count + (1 if op.produces_output else 0)
-        return self.outputs
+        return
+
+def get_program():
+    return Program(
+                """
+3,8,1001,8,10,8,105,1,0,0,21,42,67,84,109,122,203,284,365,446,99999,3,9,1002,9,3,9,1001,9,5,9,102,4,9,9,1001,9,3,9,4,9,99,3,9,1001,9,5,9,1002,9,3,9,1001,9,4,9,102,3,9,9,101,3,9,9,4,9,99,3,9,101,5,9,9,1002,9,3,9,101,5,9,9,4,9,99,3,9,102,5,9,9,101,5,9,9,102,3,9,9,101,3,9,9,102,2,9,9,4,9,99,3,9,101,2,9,9,1002,9,3,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99
+
+                """
+            )
 
 if __name__ == "__main__":
     best_score = (-1, (-1))
-    for perm in itertools.permutations(range(5)):
-        out = 0
-        for i in perm:
-            pgm = Program(
-                """
-3,8,1001,8,10,8,105,1,0,0,21,42,67,84,109,122,203,284,365,446,99999,3,9,1002,9,3,9,1001,9,5,9,102,4,9,9,1001,9,3,9,4,9,99,3,9,1001,9,5,9,1002,9,3,9,1001,9,4,9,102,3,9,9,101,3,9,9,4,9,99,3,9,101,5,9,9,1002,9,3,9,101,5,9,9,4,9,99,3,9,102,5,9,9,101,5,9,9,102,3,9,9,101,3,9,9,102,2,9,9,4,9,99,3,9,101,2,9,9,1002,9,3,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99
-                """
-            )
-            out = pgm.run(i, out)[0]
-        score = (out, perm)
+    amp_count = 5
+    for perm in itertools.permutations(range(5,10)):
+        qs = [
+            queue.Queue() for _ in range(amp_count)
+        ]
+        with ThreadPoolExecutor(max_workers = amp_count) as executor:
+            initial_input = 0
+            funcs = []
+            for i, p in enumerate(perm):
+                inq = qs[i]
+                outq = qs[(i + 1) % len(qs)]
+                qs[i].put(p)
+                if initial_input is not None:
+                    qs[i].put(initial_input)
+                    initial_input = None
+            for i, q in enumerate(qs):
+                executor.submit(get_program().run, q, qs[(i + 1) % len(qs)])
+        score = (qs[0].queue.pop(), perm)
         if score > best_score:
             best_score = score
     print(best_score)
