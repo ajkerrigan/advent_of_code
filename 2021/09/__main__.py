08cfe934aba9827bd4fb8244c6ef807c74fd1c20
AJ Kerrigan <aj@stacklet.io>
2021-12-09 20:45:38 -0500

    Docstrings for day 9, part 2

--- 2021/09/__main__.py
+++ 2021/09/__main__.py
@@ -4,6 +4,7 @@ from functools import reduce
 
 
 def populate_grid(data):
+    """Build a heightmap grid mapping (x,y) coordinates to height"""
     return {
         (x, y): int(col)
         for y, row in enumerate(data)
@@ -12,11 +13,21 @@ def populate_grid(data):
 
 
 def get_neighbors(point):
+    """Helper to get neighbor coordinates for a given point
+
+    Note: These coordinates are not guaranteed to exist on
+    the grid
+    """
     x, y = point
     return ((x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y))
 
 
 def find_low_points(grid):
+    """Find points in the grid with values lower than all neighbors
+
+    Neighbors does not include diagonals, and does not wrap
+    around edges/corners
+    """
     return {
         k: v
         for k, v in grid.items()
@@ -25,6 +36,11 @@ def find_low_points(grid):
 
 
 def find_low_neighbors(grid, point, value):
+    """Find neighboring points that belong to the same basin
+
+    Neighbors belong to the same basin if they are higher, but
+    not max height (max height: 9)
+    """
     return {point} | {
         low_neighbor
         for neighbor_k in get_neighbors(point)
@@ -34,6 +50,7 @@ def find_low_neighbors(grid, point, value):
 
 
 def find_basin_sizes(grid, low_points):
+    """Return a list of basin sizes for all low points"""
     return [len(find_low_neighbors(grid, k, v)) for k, v in low_points.items()]
 
 
