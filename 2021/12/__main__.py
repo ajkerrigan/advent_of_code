7280e45f338510c7f4cde7c817977a1062c5b182
AJ Kerrigan <aj@speckledmonkey.com>
2021-12-12 01:27:17 -0500

    Day 12, part 2

--- 2021/12/__main__.py
+++ 2021/12/__main__.py
@@ -11,19 +11,29 @@ for line in sys.stdin.readlines():
 paths = set()
 
 
-def walk(graph, path, source, dest):
+def walk(graph, path, source, dest, small_visited_twice):
+    small_visits_allowed = 2 if not small_visited_twice else 1
     options = [
         node
         for node in graph[source]
-        if not (node == "start" or (node.islower() and node in path))
+        if not (
+            node == "start"
+            or (node.islower() and path.count(node) >= small_visits_allowed)
+        )
     ]
     for opt in options:
         if opt == dest:
             paths.add(path + (opt,))
         else:
-            walk(graph, path + (opt,), opt, dest)
+            walk(
+                graph,
+                path + (opt,),
+                opt,
+                dest,
+                small_visited_twice or (opt.islower() and opt in path),
+            )
 
 
-walk(graph, ("start",), "start", "end")
+walk(graph, ("start",), "start", "end", False)
 print("\n".join(str(p) for p in paths))
 print(len(paths))
