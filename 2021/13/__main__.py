3681bde383838056677d170b39f206dab4005d81
AJ Kerrigan <aj@stacklet.io>
2021-12-13 12:20:51 -0500

    Day 13, part 2

--- 2021/13/__main__.py
+++ 2021/13/__main__.py
@@ -18,12 +18,11 @@ def populate_sparse_grid(data):
     return grid
 
 
-def print_grid(grid):
-    for y in range(grid["bounds"][1]):
-        for x in range(grid["bounds"][0]):
-            print("#" if grid.get((x, y)) else ".", end="")
-        print()
-    print()
+def visualize_grid(grid):
+    return "\n".join(
+        "".join("#" if grid.get((x, y)) else "." for x in range(grid["bounds"][0]))
+        for y in range(grid["bounds"][1])
+    )
 
 
 def fold_grid(grid, axis, pos):
@@ -41,13 +40,23 @@ def fold_grid(grid, axis, pos):
     new_grid["bounds"] = (xbound, ybound)
     return new_grid
 
+
 def part1(grid):
-    axis, pos = grid.get('folds', ())[0]
+    axis, pos = grid.get("folds", ())[0]
     grid = fold_grid(grid, axis, pos)
-    return len([v for v in grid.values() if v == True])
+    return len([k for k, v in grid.items() if v and isinstance(k, tuple)])
+
+
+def part2(grid):
+    for fold in grid.get("folds", ()):
+        axis, pos = fold
+        grid = fold_grid(grid, axis, pos)
+    return grid
 
 
 if __name__ == "__main__":
     data = sys.stdin.readlines()
     grid = populate_sparse_grid(data)
-    print(f'Part 1: {part1(grid)}')
+    print(f"Part 1: {part1(grid)}")
+
+    print("\n".join(["Part 2:", visualize_grid(part2(grid))]))
