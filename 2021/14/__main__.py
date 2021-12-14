be67dac7bceb6ad6f0f13bede718d97b1c82abd4
AJ Kerrigan <aj@stacklet.io>
2021-12-14 09:16:49 -0500

    Day 14, part 2 minor tidying

--- 2021/14/__main__.py
+++ 2021/14/__main__.py
@@ -1,13 +1,12 @@
 import sys
-from itertools import pairwise
 from collections import Counter
+from itertools import pairwise
 
 
 def step(pair_counts, insertion_rules):
     c = Counter()
     for k, v in pair_counts.items():
-        replacement = insertion_rules.get(k)
-        if replacement:
+        if replacement := insertion_rules.get(k):
             c[k[0] + replacement] += v
             c[replacement + k[1]] += v
         else:
@@ -15,8 +14,8 @@ def step(pair_counts, insertion_rules):
     return c
 
 
-def apply_steps(pair_counts, n=10):
-    for i in range(n):
+def apply_steps(pair_counts, n):
+    for _ in range(n):
         pair_counts = step(pair_counts, insertion_rules)
 
     c = Counter()
@@ -31,11 +30,7 @@ if __name__ == "__main__":
     template = "^" + sys.stdin.readline().strip()
     pair_counts = Counter("".join(p) for p in pairwise(template))
     insertion_rules = dict(
-        (
-            tuple(l.split(" -> "))
-            for line in sys.stdin.readlines()
-            if (l := line.strip())
-        )
+        l.split(" -> ") for line in sys.stdin.readlines() if (l := line.strip())
     )
     print(f"Part 1: {apply_steps(pair_counts, 10)}")
     print(f"Part 1: {apply_steps(pair_counts, 40)}")
