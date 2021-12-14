b3c9a6dd578451698bad428fdfe4fb14bad6b345
AJ Kerrigan <aj@stacklet.io>
2021-12-14 02:18:26 -0500

    Day 14, part 2

--- 2021/14/__main__.py
+++ 2021/14/__main__.py
@@ -2,22 +2,40 @@ import sys
 from itertools import pairwise
 from collections import Counter
 
-def step(template, insertion_rules):
-    for a, b in pairwise(template):
-        yield a
-        if (inserted := insertion_rules.get(f'{a}{b}')):
-            yield inserted
-    yield template[-1]
+
+def step(pair_counts, insertion_rules):
+    c = Counter()
+    for k, v in pair_counts.items():
+        replacement = insertion_rules.get(k)
+        if replacement:
+            c[k[0] + replacement] += v
+            c[replacement + k[1]] += v
+        else:
+            c[k] += v
+    return c
 
 
-if __name__ == '__main__':
-    template = sys.stdin.readline().strip()
-    insertion_rules = dict((
-        tuple(l.split(' -> '))
-        for line in sys.stdin.readlines()
-        if (l := line.strip())
-    ))
-    for i in range(10):
-        template = ''.join(step(template, insertion_rules))
-        counts = Counter(template).most_common()
-        print(counts[0][1] - counts[-1][1])
+def apply_steps(pair_counts, n=10):
+    for i in range(n):
+        pair_counts = step(pair_counts, insertion_rules)
+
+    c = Counter()
+    for k, v in pair_counts.most_common():
+        c[k[1]] += v
+
+    counts = c.most_common()
+    return counts[0][1] - counts[-1][1]
+
+
+if __name__ == "__main__":
+    template = "^" + sys.stdin.readline().strip()
+    pair_counts = Counter("".join(p) for p in pairwise(template))
+    insertion_rules = dict(
+        (
+            tuple(l.split(" -> "))
+            for line in sys.stdin.readlines()
+            if (l := line.strip())
+        )
+    )
+    print(f"Part 1: {apply_steps(pair_counts, 10)}")
+    print(f"Part 1: {apply_steps(pair_counts, 40)}")
