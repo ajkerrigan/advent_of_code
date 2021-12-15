0134be63af22108c55ef848c6c681682b58ae8fe
AJ Kerrigan <aj@stacklet.io>
2021-12-15 15:27:56 -0500

    Oh wait this way is much quicker
    I'm not saying _good_, but much quicker :)

--- 2021/15/__main__.py
+++ 2021/15/__main__.py
@@ -2,38 +2,39 @@ import sys
 
 
 def populate_grid(data):
-    """Build a heightmap grid mapping (x,y) coordinates to height"""
     return {
         (x, y): int(col)
         for y, row in enumerate(data)
         for x, col in enumerate(row.strip())
     }
 
+
 def get_neighbors(grid, point):
     def _get_neighbors(point):
         x, y = point
-        return sorted(
-            ((k, grid[k])
-            for k in
-            (
-                (x, y + 1), (x + 1, y), (x - 1, y), (x, y - 1)
-            )
-            if k in grid),
-            key=lambda x: (x[1], -(x[0][0]), -(x[0][1]))
+        return (
+            (k, grid[k])
+            for k in ((x, y + 1), (x + 1, y), (x - 1, y), (x, y - 1))
+            if k in grid
         )
+
     return _get_neighbors(point)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     grid = populate_grid(sys.stdin)
     shortest_grid = {(0, 0): 0}
-    size = int(len(grid) ** (1/2))
-    end = (size-1, size-1)
+    size = int(len(grid) ** (1 / 2))
+    end = (size - 1, size - 1)
     while True:
-        for point, risk in shortest_grid.copy().items():
-            for n in (n for n in get_neighbors(grid, point) if n[0] not in shortest_grid):
-                possible_risk = (shortest_grid.get(p[0], 1_000_000) for p in get_neighbors(grid, n[0]))
-                shortest_grid[n[0]] = n[1] + min(possible_risk)
+        for point, risk in grid.items():
+            possible_risk = [
+                shortest_grid.get(p[0])
+                for p in get_neighbors(grid, point)
+                if p[0] in shortest_grid
+            ]
+            if possible_risk:
+                shortest_grid[point] = min(possible_risk) + risk
         if len(grid) == len(shortest_grid):
             break
     print(shortest_grid[end])
