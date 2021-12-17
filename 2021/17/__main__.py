5b8929c1786e096cd2f7271d5390d65741222507
AJ Kerrigan <aj@stacklet.io>
2021-12-17 15:29:51 -0500

    Day 17, part 2
    Added an automatic solution, left the interactive mode in because it was
    fun.
    
    Took too long to notice that I was ignoring hits with a max height of 0
    because it was falsy. Graaaaaah.

--- 2021/17/__main__.py
+++ 2021/17/__main__.py
@@ -1,3 +1,4 @@
+import os
 import re
 import sys
 from itertools import product
@@ -7,7 +8,7 @@ from operator import itemgetter
 def step(position, velocity):
     x1, y1 = position
     vx, vy = velocity
-    return ((x1 + vx, y1 + vy), (vx and vx - (vx / abs(vx)), vy - 1))
+    return ((x1 + vx, y1 + vy), (vx and vx - int(vx / abs(vx)), vy - 1))
 
 
 def visualize_grid(grid):
@@ -19,18 +20,19 @@ def visualize_grid(grid):
     )
 
 
-def fire(grid, target_bounds, velocity):
+def fire(grid, target_bounds, velocity, visualize=False):
     pos = (0, 0)
     hit = False
     xbound, ybound = target_bounds
+    v = velocity
 
-    while pos[0] < max(xbound) and pos[1] > min(ybound):
-        pos, velocity = step(pos, velocity)
+    while pos[0] < max(xbound) + 1 and pos[1] > min(ybound) - 1:
         if grid.get((pos[0], pos[1])) == "T":
             hit = True
         grid[(pos[0], pos[1])] = "#"
-        print(pos, velocity)
-    print(visualize_grid(grid))
+        pos, v = step(pos, v)
+    if visualize:
+        print(visualize_grid(grid))
     return max(k[1] for k, v in grid.items() if v == "#") if hit else None
 
 
@@ -54,12 +56,13 @@ def initialize_grid(target_bounds):
     return grid
 
 
-if __name__ == "__main__":
-    with open(sys.argv[1]) as f:
-        target_bounds = get_target_bounds(f.read())
-    grid = initialize_grid(target_bounds)
+def interactive_fire(grid, target_bounds):
+    # Close and reopen stdin, so we can muck around with keyboard
+    # input after handling a piped input file
+    sys.stdin.close()
+    sys.stdin = os.fdopen(1)
+    velocity = (0, 0)
     max_heights = {}
-    velocity = (5, 5)
     keep_going = True
     while keep_going:
         print("Top heights so far:")
@@ -86,5 +89,34 @@ if __name__ == "__main__":
                 velocity = tweak_velocity(velocity, 0, amount * 1)
             case "l":
                 velocity = tweak_velocity(velocity, amount * 1, 0)
-        if max_height := fire(grid.copy(), target_bounds, velocity):
+        max_height = fire(grid.copy(), target_bounds, velocity, visualize=True)
+        if max_height is not None:
             max_heights[velocity] = max_height
+    return max(v for v in max_heights.values() if v), len(max_heights)
+
+
+def automatic_fire(grid, target_bounds):
+    max_heights = {}
+    xbound, ybound = target_bounds
+    vxbound = max(abs(b) for b in xbound)
+    vybound = max(abs(b) for b in ybound)
+    for x in range(0, vxbound + 1):
+        for y in range(-vybound, vybound + 1):
+            velocity = tweak_velocity((0, 0), x, y)
+            max_height = fire(grid.copy(), target_bounds, velocity)
+            if max_height is not None:
+                max_heights[velocity] = max_height
+    return max(v for v in max_heights.values() if v), len(max_heights)
+
+
+if __name__ == "__main__":
+    target_bounds = get_target_bounds(sys.stdin.read())
+    grid = initialize_grid(target_bounds)
+    start_firing = (
+        interactive_fire
+        if len(sys.argv) == 2 and sys.argv[1] == "--interactive"
+        else automatic_fire
+    )
+    max_height, unique_solutions = start_firing(grid, target_bounds)
+    print(f"Part 1: {max_height}")
+    print(f"Part 2: {unique_solutions}")
