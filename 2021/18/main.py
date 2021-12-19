63d44dbe14d35555ff007ea35c0392a709bb02cf
AJ Kerrigan <aj@speckledmonkey.com>
2021-12-18 23:47:16 -0500

    Checkpoint before rethinking

--- 2021/18/main.py
+++ 2021/18/main.py
@@ -1,19 +1,64 @@
 from __future__ import annotations
-
+from math import ceil
 import sys
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Snailfish:
     left: int | Snailfish | None = None
     right: int | Snailfish | None = None
+    _left: int | Snailfish | None = field(init=False)
+    _right: int | Snailfish | None = field(init=False)
+    parent: Snailfish | None = field(init=False)
+
+    @property
+    def left(self):
+        return self._left
+
+    @property
+    def right(self):
+        return self._right
+
+    @left.setter
+    def left(self, val):
+        if isinstance(val, self.__class__):
+            val.parent = self
+        self._left = val
+
+    @right.setter
+    def right(self, val):
+        if isinstance(val, self.__class__):
+            val.parent = self
+        self._right = val
+
 
     def __str__(self):
         return f'[{self.left},{self.right}]'
 
     def has_children(self):
         return isinstance(self.left, self.__class__) or isinstance(self.right, self.__class__)
+
+    def add_exploded_value(self, value, side):
+        if side == 'right':
+            if isinstance(self.left, int):
+                self.left += value
+                return True
+            elif self.parent.add_exploded_value(value, side):
+                return True
+            if isinstance(self.right, int):
+                self.right += value
+                return True
+            return self.right.add_exploded_value(value, side)    
+        if isinstance(self.right, int):
+            self.right += value
+            return True
+        elif self.right.add_exploded_value(value, side):
+            return True
+        if isinstance(self.left, int):
+            self.left += value
+            return True
+        return self.left.add_exploded_value(value, side)    
 
     def explode(self, level=0):
         exploded = None
@@ -33,22 +78,51 @@ class Snailfish:
                     exploded = (self.right.left, self.right.right)
                 self.right = 0
             return exploded
-        vals = self.left.explode(level + 1) if isinstance(self.left, self.__class__) else self.right.explode(level + 1) if isinstance(self.right, self.__class__) else None
+        vals = (isinstance(self.left, self.__class__) and self.left.explode(level + 1)) or (isinstance(self.right, self.__class__) and self.right.explode(level + 1))
+        if not vals:
+            return
         left, right = vals
-        if right and isinstance(self.right, int):
-            self.right += right
+        if right:
+            if isinstance(self.right, int):
+                self.right += right
+            elif isinstance(self.left, self.__class__):
+                self.parent.add_exploded_value(right, 'right')
             right = None
-        if left and isinstance(self.left, int):
-            self.left += left
+        if left:
+            if isinstance(self.left, int):
+                self.left += left
+            else:
+                self.parent.add_exploded_value(left, 'left')
             left = None
         return left, right
 
     def split(self):
-        return NotImplementedError
+        if isinstance(self.left, int) and self.left >= 10:
+            self.left = self.__class__(self.left // 2, ceil(self.left / 2))
+            return True
+        if isinstance(self.right, int) and self.right >= 10:
+            self.right = self.__class__(self.right // 2, ceil(self.right / 2))
+            return True
+        split = False
+        if isinstance(self.left, self.__class__):
+            split = self.left.split()
+        if not split and isinstance(self.right, self.__class__):
+            split = self.right.split()
+        return split
+        
+
+    def __add__(self, other):
+        return self.__class__(self, other)
 
     def reduce(self):
-        self.explode()
-        self.split()
+        while True:
+            start = str(self)
+            self.explode()
+            if str(self) != start:
+                continue
+            self.split()
+            if str(self) == start:
+                break
 
     @classmethod
     def parse(cls, str_):
--- 2021/18/test_day18.py
+++ 2021/18/test_day18.py
@@ -20,9 +20,53 @@ def test_roundtrip():
         ('[[[[[9,8],1],2],3],4]', '[[[[0,9],2],3],4]'),
         ('[7,[6,[5,[4,[3,2]]]]]', '[7,[6,[5,[7,0]]]]'),
         ('[[6,[5,[4,[3,2]]]],1]', '[[6,[5,[7,0]]],3]'),
+        ('[[[[0,7],4],[7,[[8,4],9]]],[1,1]]', '[[[[0,7],4],[15,[0,13]]],[1,1]]'),
+        ('[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]', '[[[[0,7],4],[[7,8],[6,0]]],[8,1]]')
     ]
 )
-def test_exploding(start, exploded):
+def test_explode(start, exploded):
     fish = Snailfish.parse(start)
     fish.explode()
     assert str(fish) == str(Snailfish.parse(exploded))
+
+@pytest.mark.parametrize(
+    'left, right, expected',
+    [
+        ('[[[[4,3],4],4],[7,[[8,4],9]]]', '[1,1]', '[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]')
+    ]
+)
+def test_add(left, right, expected):
+    fish = Snailfish.parse(left) + Snailfish.parse(right)
+    assert str(fish) == expected
+
+@pytest.mark.parametrize(
+    'left, right, expected',
+    [
+        ('[[[[4,3],4],4],[7,[[8,4],9]]]', '[1,1]', '[[[[0,7],4],[[7,8],[6,0]]],[8,1]]')
+    ]
+)
+def test_add_reduce(left, right, expected):
+    added = Snailfish.parse(left) + Snailfish.parse(right)
+    added.reduce()
+    assert str(added) == expected
+
+@pytest.mark.parametrize(
+    'pair, expected', [
+        ('[[[[0,7],4],[15,[0,13]]],[1,1]]', '[[[[0,7],4],[[7,8],[0,13]]],[1,1]]'),
+        ('[[[[0,7],4],[[7,8],[0,13]]],[1,1]]', '[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]')
+    ]
+)
+def test_split(pair, expected):
+    fish = Snailfish.parse(pair)
+    fish.split()
+    assert str(fish) == expected
+
+@pytest.mark.parametrize(
+    'pair, expected', [
+        ('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]', '[[[[0,7],4],[[7,8],[6,0]]],[8,1]]')
+    ]
+)
+def test_reduce(pair, expected):
+    fish = Snailfish.parse(pair)
+    fish.reduce()
+    assert str(fish) == expected
