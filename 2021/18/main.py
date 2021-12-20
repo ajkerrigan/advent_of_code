211d0b21b2da07ff2af5d5d33f9f83014666c8e3
AJ Kerrigan <aj@stacklet.io>
2021-12-20 17:27:30 -0500

    Day 18 part 1

--- 2021/18/main.py
+++ 2021/18/main.py
@@ -1,80 +1,91 @@
 from __future__ import annotations
-from math import ceil
-from itertools import chain
+
 import sys
-from dataclasses import dataclass, field
 from contextlib import suppress
+from dataclasses import dataclass, field
+from math import ceil
+
 
 @dataclass
 class Node:
     value: int | Snailfish
-    _value: int | Snailfish | None = field(init=False, default=None)
+    parent: Snailfish | None = field(
+        init=False, repr=False, default=None, compare=False
+    )
 
-    def is_regular(self):
-        return isinstance(self.value, int)
     def __str__(self):
         return str(self.value)
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, val):
-        self._value = val
-    
-    @property
-    def parent(self):
-        return self.value.parent if isinstance(self.value, Snailfish) else None
-    @parent.setter
-    def parent(self, val):
-        if isinstance(self, Snailfish):
-            self.value.parent = val
 
 
 @dataclass
 class Snailfish:
-    left: int | Snailfish | None = field(init=False)
-    right: int | Snailfish | None = field(init=False)
-    value: int | None = None
-    _left: int | Snailfish | None = field(init=False)
-    _right: int | Snailfish | None = field(init=False)
-    node: Node | None = field(init=False)
-    parent: Snailfish | None = field(init=False, repr=False)
+    _left: Node | Snailfish | None = field(repr=False, default=None)
+    _right: Node | Snailfish | None = field(repr=False, default=None)
+    parent: Snailfish | None = field(
+        init=False, repr=False, default=None, compare=False
+    )
+
+    def __post_init__(self):
+        self.__class__.left = property(self.__class__.get_left, self.__class__.set_left)
+        self.__class__.right = property(
+            self.__class__.get_right, self.__class__.set_right
+        )
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.left},{self.right})"
 
     def is_regular(self):
         return False
 
-    @property
-    def left(self):
+    def get_left(self):
         return self._left
 
-    @property
-    def right(self):
+    def get_right(self):
         return self._right
 
-    @left.setter
-    def left(self, val):
-        val.parent = self
+    def set_left(self, val):
+        if isinstance(val, int):
+            val = Node(val)
+        if isinstance(val, (Node, Snailfish)):
+            val.parent = self
         self._left = val
 
-    @right.setter
-    def right(self, val):
-        val.parent = self
+    def set_right(self, val):
+        if isinstance(val, int):
+            val = Node(val)
+        if isinstance(val, (Node, Snailfish)):
+            val.parent = self
         self._right = val
 
     def __str__(self):
-        return f'[{self.left},{self.right}]'
+        return f"[{self.left},{self.right}]"
 
     def flatten(self):
         flat = []
-        if self.left:
-            flat.extend(self.left.flatten())
-        if self.value:
-            flat.append(self)
-        if self.right:
-            flat.extend(self.right.flatten())
+        flat.extend(
+            self.left.flatten() if isinstance(self.left, Snailfish) else [self.left]
+        )
+        flat.extend(
+            self.right.flatten() if isinstance(self.right, Snailfish) else [self.right]
+        )
         return flat
+
+    def magnitude(self):
+        left = (
+            self.left.value
+            if isinstance(self.left, Node)
+            else self.left.magnitude()
+            if isinstance(self.left, self.__class__)
+            else 0
+        )
+        right = (
+            self.right.value
+            if isinstance(self.right, Node)
+            else self.right.magnitude()
+            if isinstance(self.right, self.__class__)
+            else 0
+        )
+        return 3 * left + 2 * right
 
     def explode(self):
         flat = list(self.flatten())
@@ -82,38 +93,48 @@ class Snailfish:
         index = 0
         for i, node in enumerate(flat):
             with suppress(AttributeError):
-                if node.parent.parent.parent:
+                if node.parent.parent.parent.parent.parent:
                     target = node.parent
                     index = i
                     break
         if target:
             if index - 1 >= 0:
                 flat[index - 1].value += target.left.value
-            if index + 1 < len(flat):
-                flat[index + 1].value += target.right.value
-            target = Node(0)
-                
-                    
-
-
+            if index + 2 < len(flat):
+                flat[index + 2].value += target.right.value
+            if target.parent.left is target:
+                target.parent.left = Node(0)
+            if target.parent.right is target:
+                target.parent.right = Node(0)
 
     def split(self):
-        if self.left.is_regular() and self.left.value >= 10:
-            self.left = self.__class__(self.left.value // 2, ceil(self.left.value / 2))
+        if isinstance(self.left, Node) and self.left.value >= 10:
+            val = self.left.value
+            self.left = self.__class__()
+            self.left.left = Node(val // 2)
+            self.left.right = Node(ceil(val / 2))
             return True
-        if self.right.is_regular() and self.right.value >= 10:
-            self.right = self.__class__(self.right.value // 2, ceil(self.right.value / 2))
+        if isinstance(self.left, Snailfish):
+            if self.left.split():
+                return True
+        if isinstance(self.right, Node) and self.right.value >= 10:
+            val = self.right.value
+            self.right = self.__class__()
+            self.right.left = Node(val // 2)
+            self.right.right = Node(ceil(val / 2))
             return True
-        split = False
-        if not self.left.is_regular():
-            split = self.left.value.split()
-        if not split and not self.right.is_regular():
-            split = self.right.value.split()
-        return split
-        
+        if isinstance(self.right, Snailfish):
+            if self.right.split():
+                return True
 
     def __add__(self, other):
-        return self.__class__(self, other)
+        if self.left or self.right:
+            newfish = self.__class__()
+            newfish.left = Snailfish.parse(str(self))
+            newfish.right = Snailfish.parse(str(other))
+            newfish.reduce()
+            return newfish
+        return other
 
     def reduce(self):
         while True:
@@ -131,27 +152,39 @@ class Snailfish:
         numbers = []
         for t in str_:
             match t:
-                case '[':
+                case "[":
                     fish.append(cls())
                 case num if str.isnumeric(num):
                     numbers.append(num)
-                case ']':
+                case "]":
                     if numbers:
-                        fish[-1].right = int(str(''.join(numbers)))
+                        fish[-1].right = int(str("".join(numbers)))
                         numbers = []
                     else:
                         top = fish.pop()
                         fish[-1].right = top
                     if len(fish) == 1:
                         return fish.pop()
-                case ',':
+                case ",":
                     if numbers:
-                        fish[-1].left = int(str(''.join(numbers)))
+                        fish[-1].left = int(str("".join(numbers)))
                         numbers = []
                     else:
                         top = fish.pop()
                         fish[-1].left = top
 
-if __name__ == '__main__':
-    for line in sys.stdin.readlines():
-        print(Snailfish.parse(line.strip()))
+
+if __name__ == "__main__":
+    fish = None
+    # for line in sys.stdin.readlines():
+    #     newfish = Snailfish.parse(line.strip())
+    #     print(f'  {fish}')
+    #     print(f'+ {newfish}')
+    #     fish = newfish if fish is None else (fish + newfish)
+    #     print(f'= {fish}')
+    fish = sum(
+        (Snailfish.parse(line.strip()) for line in sys.stdin.readlines()),
+        start=Snailfish(),
+    )
+    print(f"Final sum: {fish}")
+    print(fish.magnitude())
