02a4f3ba5e8a0f5b743e0e20ae898db0fb854849
AJ Kerrigan <aj@stacklet.io>
2022-12-04 16:21:01 -0500

    Day 4 part 2
    ...and refactor part 1

--- 2022/04/__main__.py
+++ 2022/04/__main__.py
@@ -2,28 +2,63 @@ import logging
 import sys
 
 
-def part1(data):
-    fully_contained_overlaps = 0
-    for pair in data.splitlines():
-        elf1, elf2 = [
-            tuple(int(n) for n in assignment.split("-"))
-            for assignment in pair.split(",")
-        ]
-        combined = sorted(elf1 + elf2)
+def assignment_to_set(assignment: str) -> set:
+    """Convert a section assignment range to a set of sections
 
-        # Does one elf have both the highest and lowest
-        # section numbers?
-        if (combined[0], combined[-1]) in (elf1, elf2):
-            fully_contained_overlaps += 1
-    return fully_contained_overlaps
+    >>> assignment_to_set("2-4")
+    {2, 3, 4}
+    """
+    low, high = tuple(int(n) for n in assignment.split("-"))
+    return set(range(low, high + 1))
 
 
-def part2(data):
-    ...
+def parse_assignments(data: str) -> list[tuple[set]]:
+    """Split cleanup assignment data into paired sets
+    of assigned sections.
+
+    assignments = '''
+    2-4,6-8,
+    2-5,3-7
+    '''
+    >>> parse_assignments(assignments)
+    [
+        ({2,3,4}, {6,7,8}),
+        ({2,3,4,5}, {3,4,5,6,7})
+    ]
+    """
+    return [
+        tuple(assignment_to_set(assignment) for assignment in pair.split(","))
+        for pair in data.splitlines()
+    ]
+
+
+def part1(assignments) -> int:
+    """Return the number of assignments where one elf's section set
+    completely overlaps the other.
+
+    To determine complete overlap, see if the union of assignments
+    matches either single elf's assignments.
+
+    Use sum to count matches, since True == 1 and False == 0
+    """
+    return sum(elf1 | elf2 in (elf1, elf2) for elf1, elf2 in assignments)
+
+
+def part2(assignments) -> int:
+    """Return the number of elf pairs where any section assignments
+    overlap.
+
+    Sets that are disjoint don't overlap. So if sets are _not_
+    disjoint, they _do_ overlap.
+
+    Use sum to count matches, since True == 1 and False == 0
+    """
+    return sum(not set.isdisjoint(*assignment) for assignment in assignments)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     data = sys.stdin.read()
-    print(f"Part 1: {part1(data)}")
-    print(f"Part 2: {part2(data)}")
+    assignments = parse_assignments(data)
+    print(f"Part 1: {part1(assignments)}")
+    print(f"Part 2: {part2(assignments)}")
