58743844fcd09098658635a6e60a2c5b52b0a1b3
AJ Kerrigan <aj@stacklet.io>
2022-12-05 01:30:20 -0500

    Day 5 part 2

--- 2022/05/__main__.py
+++ 2022/05/__main__.py
@@ -14,28 +14,33 @@ def build_initial_position(starting_stacks):
     return stacks
 
 
-def move_crates(stacks, moves):
+def move_crates(stacks, moves, model9000=True):
     new_stacks = deepcopy(stacks)
     for move in moves:
         num_crates, move_from, move_to = (int(n) for n in re.findall(r"\d+", move))
-        for _ in range(num_crates):
-            new_stacks[move_to - 1].append(new_stacks[move_from - 1].pop())
+        stay, go = (
+            new_stacks[move_from - 1][:-num_crates],
+            new_stacks[move_from - 1][-num_crates:],
+        )
+        new_stacks[move_from - 1] = stay
+        new_stacks[move_to - 1].extend(reversed(go) if model9000 else go)
     return new_stacks
 
 
-def part1(data):
-    starting_stacks, moves = (x.splitlines() for x in data.split("\n\n"))
-    stacks = build_initial_position(starting_stacks)
+def part1(stacks, moves):
     final_position = move_crates(stacks, moves)
     return "".join(stack[-1] for stack in final_position.values())
 
 
-def part2(data):
-    ...
+def part2(stacks, moves):
+    final_position = move_crates(stacks, moves, model9000=False)
+    return "".join(stack[-1] for stack in final_position.values())
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     data = sys.stdin.read()
-    print(f"Part 1: {part1(data)}")
-    print(f"Part 2: {part2(data)}")
+    starting_stacks, moves = (x.splitlines() for x in data.split("\n\n"))
+    stacks = build_initial_position(starting_stacks)
+    print(f"Part 1: {part1(stacks, moves)}")
+    print(f"Part 2: {part2(stacks, moves)}")
