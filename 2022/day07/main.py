a8d14c23e7e5fae6cd90b61d74889fba3cd735d7
AJ Kerrigan <aj@stacklet.io>
2022-12-07 12:51:58 -0500

    2022 day 7 part 2

--- 2022/day07/main.py
+++ 2022/day07/main.py
@@ -22,25 +22,54 @@ def parse_term_output(data):
 
 
 @dataclass
-class Entry:
+class FilesystemEntry:
+    """A single filesystem object"""
+
     name: str
+
+    @property
+    def type(self):
+        raise NotImplemented
 
 
 @dataclass
-class Directory(Entry):
-    contents: list[Entry] = field(default_factory=list)
+class File(FilesystemEntry):
+    """A single file in a filesystem"""
+
+    size: int
+
+    @property
+    def type(self):
+        return "file"
+
+
+@dataclass
+class Directory(FilesystemEntry):
+    """A directory in a filesystem
+
+    Directories contain 0 or more children.
+    """
+
+    contents: list[FilesystemEntry] = field(default_factory=list)
 
     @property
     def size(self):
+        """The total size of a directory's contents."""
         return sum(c.size for c in self.contents)
 
+    @property
+    def type(self):
+        return "directory"
 
-@dataclass
-class File(Entry):
-    size: int
 
+def build_filesystem(commands: list) -> dict[str, FilesystemEntry]:
+    """Build a filesystem using command output
 
-def build_filesystem(commands: list):
+    Iterate over a list of commands and their
+    output, using it to build a dict whose keys
+    are path names and values are file/directory
+    details.
+    """
     fs = {}
     pwd = Directory("/")
     fs[pwd.name] = pwd
@@ -50,31 +79,35 @@ def build_filesystem(commands: list):
             case "ls":
                 for line in output:
                     size, name = line
+                    entry = str(Path(pwd.name) / name)
                     if size == "dir":
-                        entry = str(Path(pwd.name) / name)
                         pwd.contents.append(fs.setdefault(entry, Directory(entry)))
                     else:
-                        entry = str(Path(pwd.name) / name)
                         pwd.contents.append(
                             fs.setdefault(entry, File(entry, int(size)))
                         )
             case "cd":
                 if args[0] == "..":
-                    new_pwd = pwd.name.rpartition("/")[0]
+                    new_pwd = str(Path(pwd.name).parent)
                 elif "/" in args[0]:
-                    new_pwd = args[0]
+                    new_pwd = str(Path(args[0]))
                 else:
                     new_pwd = str(Path(pwd.name) / args[0])
                 pwd = fs.setdefault(new_pwd, Directory(new_pwd))
     return fs
 
 
-def part1(data: str):
+def part1(data: str) -> int:
+    """Sum of small directories
+
+    Sum the sizes of all directories whose
+    individual sizes are less than 100000.
+    """
     parsed = parse_term_output(data)
     fs = build_filesystem(parsed)
     total_size = 0
     for entry in fs.values():
-        if isinstance(entry, File):
+        if not entry.type == "directory":
             continue
         if not (0 < entry.size <= 100000):
             continue
@@ -82,8 +115,25 @@ def part1(data: str):
     return total_size
 
 
-def part2(data: str):
-    ...
+def part2(data: str) -> int:
+    """Delete a directory to save space
+
+    Find the smallest individual directory
+    that can be deleted to free up enough space
+    to install an update.
+    """
+    parsed = parse_term_output(data)
+    fs = build_filesystem(parsed)
+    required_space = 30000000
+    total_space = 70000000
+    free_space = total_space - fs["/"].size
+    space_to_free = required_space - free_space
+    candidate_sizes = [
+        entry.size
+        for entry in fs.values()
+        if isinstance(entry, Directory) and entry.size >= space_to_free
+    ]
+    return min(candidate_sizes)
 
 
 if __name__ == "__main__":
