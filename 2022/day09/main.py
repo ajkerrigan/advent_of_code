c324948e72f8fdda8afb8b3c9b5d707015b7dc1f
AJ Kerrigan <aj@stacklet.io>
2022-12-09 12:46:09 -0500

    day 9 part 1 cleanup

--- 2022/day09/main.py
+++ 2022/day09/main.py
@@ -1,43 +1,7 @@
 import sys
-from dataclasses import dataclass
-from enum import Enum
 from operator import attrgetter
 
-
-@dataclass
-class Point:
-    x: int
-    y: int
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
-
-    def close_gap(self, other):
-        dx, dy = other.x - self.x, other.y - self.y
-        if abs(dx) <= 1 and abs(dy) <= 1:
-            return self
-        if abs(dx) > 0 and abs(dy) > 0:
-            return Point(self.x + (1 if dx > 0 else -1), self.y + (1 if dy > 0 else -1))
-        elif abs(dx) > 1:
-            return Point(self.x + (1 if dx > 0 else -1), self.y)
-        elif abs(dy) > 1:
-            return Point(self.x, self.y + (1 if dy > 0 else -1))
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-
-class Direction(Enum):
-    R = Point(1, 0)
-    L = Point(-1, 0)
-    U = Point(0, -1)
-    D = Point(0, 1)
+from coordinates import Direction, Point
 
 
 def parse_instructions(data: str) -> list:
@@ -64,13 +28,14 @@ def print_visits(visited):
 
 def part1(data: str) -> int:
     instructions = parse_instructions(data)
-
     head, tail = Point(0, 0), Point(0, 0)
     visited = set()
+
     for inst in instructions:
         head += inst
         tail = tail.close_gap(head)
         visited.add(tail)
+
     print_visits(visited)
     return len(visited)
 
