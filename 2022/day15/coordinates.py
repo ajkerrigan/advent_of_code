4ea281006cead00631b29420856782bff0c90ac1
AJ Kerrigan <aj@stacklet.io>
2022-12-15 11:56:11 -0500

    day 15 checkpoint

--- 2022/day15/coordinates.py
+++ 2022/day15/coordinates.py
@@ -8,6 +8,7 @@ class Mark(StrEnum):
     SENSOR = "S"
     BEACON = "B"
     AIR = "."
+    NO_BEACON = "#"
 
 
 @dataclass
@@ -30,11 +31,20 @@ class Point:
     def coords(self):
         return (self.x, self.y)
 
+    def __iter__(self):
+        return iter(self.coords)
+
+    def taxi_distance(self, other):
+        return sum(abs(x - y) for x, y in zip(iter(self), iter(other)))
+
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
         return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Point(self.x * other, self.y * other)
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
--- 2022/day15/main.py
+++ 2022/day15/main.py
@@ -2,7 +2,7 @@ import logging
 import os
 import re
 import sys
-
+from itertools import product, islice
 from coordinates import Mark, Point
 
 logging.basicConfig(
@@ -28,12 +28,45 @@ def print_grid(grid):
     log = logging.getLogger("print_grid")
     (xmin, xmax), (ymin, ymax) = ((min(pos), max(pos) + 1) for pos in zip(*grid))
     for y in range(ymin, ymax):
-        log.debug("".join(str(grid.get((x, y), Mark.AIR)) for x in range(xmin, xmax)))
+        log.debug(
+            f"{y:-{len(str(ymax)) + 1}}"
+            + "".join(str(grid.get((x, y), Mark.AIR)) for x in range(xmin, xmax))
+        )
+
+
+def beacon_sweep(grid, row):
+    sensors = [
+        point
+        for point in grid.values()
+        if point.mark == Mark.SENSOR
+        and abs(row - point.y) <= point.taxi_distance(point.buddy)
+    ]
+    log = logging.getLogger("beacon_sweep")
+    for sensor in sensors:
+        log.debug("sensor: %s", sensor.coords)
+        beacon_distance = sensor.taxi_distance(sensor.buddy)
+        log.debug("beacon distance: %d", beacon_distance)
+        x_coords = [
+            x for x in range(sensor.x - beacon_distance, sensor.x + beacon_distance)
+        ]
+        log.debug("checking %d coords: %r ...", len(x_coords), x_coords[:10])
+        for x_coord in x_coords:
+            if sensor.taxi_distance((x_coord, row)) > beacon_distance:
+                continue
+            grid.setdefault((x_coord, row), Point(x_coord, row, Mark.NO_BEACON))
+    return grid
+
+
+def sweep_row(grid, row):
+    return sum(v.mark == Mark.NO_BEACON for k, v in grid.items() if k[1] == row)
 
 
 def part1(data: str) -> int:
     grid = build_grid(data)
+    row = 10 if len(grid) == 20 else 2_000_000
+    grid = beacon_sweep(grid, row)
     print_grid(grid)
+    return sweep_row(grid, row)
 
 
 def part2(data: str) -> int:
